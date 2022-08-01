e5d93c13a189e676da60785113fec3a56ff079ba
Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>
2022-08-01 16:02:24 -0700

    fix launch code

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -16,43 +16,43 @@ export default function Launch() {
             authURL = `${process.env.REACT_APP_BACKEND_URL}/auth/auth-info`;
         }
         const urlParams = new URLSearchParams(window.location.search);
-         //retrieve patient id from URL querystring if any
+        //retrieve patient id from URL querystring if any
         let patientId = urlParams.get('patient');
         console.log("patient id from url query string: ", patientId);
-	    console.log("authURL: ", authURL);
-    
+        console.log("authURL: ", authURL);
+
         fetch(authURL, {
             // include cookies in request
             credentials: 'include'
         })
-        .then(result => {
-            if (!result.ok) {
-                throw new Error(result.status.toString());
-            }
-            return result.json();
-        })
-        .catch(e => setError(e))
-        .then(json => {
-            if (patientId) {
-                //only do this IF patient id comes from url queryString
-                json.patientId = patientId;
-                sessionStorage.setItem(queryPatientIdKey, patientId);
-            }
-	    //allow auth scopes to be updated via environment variable
-	    //see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
-	    const envAuthScopes = process.env.REACT_APP_AUTH_SCOPES;
-	    if (envAuthScopes) json.scope = envAuthScopes;
-	    
-            console.log("launch context json ", json);
-            FHIR.oauth2.authorize(json).catch((e) => {
-                setError(e);
-            });
+            .then(result => {
+                if (!result.ok) {
+                    throw new Error(result.status.toString());
+                }
+                return result.json();
+            })
+            .catch(e => setError(e))
+            .then(json => {
+                if (patientId) {
+                    //only do this IF patient id comes from url queryString
+                    json.patientId = patientId;
+                    sessionStorage.setItem(queryPatientIdKey, patientId);
+                }
+                //allow auth scopes to be updated via environment variable
+                //see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
+                const envAuthScopes = process.env.REACT_APP_AUTH_SCOPES;
+                if (envAuthScopes) json.scope = envAuthScopes;
 
-        })
-        .catch(e => {
-            setError(e);
-            console.log('launch error ', e);
-        });
+                console.log("launch context json ", json);
+                FHIR.oauth2.authorize(json).catch((e) => {
+                    setError(e);
+                });
+
+            })
+            .catch(e => {
+                setError(e);
+                console.log('launch error ', e);
+            });
     }, []);
 
     return (
--- src/util/util.js
+++ src/util/util.js
@@ -1,1 +1,6 @@
+export const getEnv = (key) => {
+  if (!process || !process.env) return "";
+  return process.env[key];
+};
+
 export const queryPatientIdKey = 'launch_queryPatientId';
