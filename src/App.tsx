bc087328d5b9a555449ed4e48e91452e308d5fa7
Hannah <ha.al.bu@gmail.com>
2022-09-14 17:51:20 -0700

    fetch env data before trying to read env variable

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,11 @@ import {EnrollmentApp} from "./components/EnrollmentApp";
 import {MessagingApp} from "./components/MessagingApp";
 import {fetchEnvData, getEnv} from "./util/util";
 
+fetchEnvData();
+const REACT_APP_CLIENT_ID = getEnv("REACT_APP_CLIENT_ID");
 
 export default class App extends React.Component<any, any> {
-    componentDidMount() {
-        fetchEnvData();
-    }
-
     render() {
-        const REACT_APP_CLIENT_ID = getEnv("REACT_APP_CLIENT_ID");
         return (
             <ThemeProvider theme={theme}>
                 <LocalizationProvider dateAdapter={AdapterMoment}>
