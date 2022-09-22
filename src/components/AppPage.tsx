bdbf84f4ae7e98df6051209736a53c6257126f6c
Hannah <ha.al.bu@gmail.com>
2022-09-22 16:39:51 -0700

    remove usage of deprecated @mui/styles api;

--- src/components/AppPage.tsx
+++ src/components/AppPage.tsx
@@ -1,72 +1,58 @@
 import React, {FunctionComponent} from "react";
 import DemoVersionBanner from "./DemoVersionBanner";
-import {makeStyles} from "@mui/styles";
-import {Divider, Theme, Typography} from "@mui/material";
+import {Divider, Typography} from "@mui/material";
 import VersionString from "./VersionString";
 import AppBanner from "./AppBanner";
+import Box from "@mui/material/Box";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    width: '100%'
-  },
-  content: {
-    "padding-top": 16,
-    "padding-left": 16,
-    "padding-right": 16,
-    "padding-bottom": 16
-  },
-  divider: {
-    "margin-top": 4,
-    "margin-bottom": 8,
-    "margin-left": 80,
-    "margin-right": 80,
-    "height":2
-  },
-  pageTitle: {
-    fontWeight: "bolder"
-  }
-}));
+const styles = {content: { "padding": "16px" }};
 
 type AppPageScaffoldProps = {
-  title?: string | JSX.Element;
-  appBarChild?: JSX.Element;
+    title?: string | JSX.Element;
+    appBarChild?: JSX.Element;
 }
 
 type PageTitleProps = {
-  title? : string | JSX.Element;
+    title?: string | JSX.Element;
 }
 
 export const PageTitle: FunctionComponent<React.PropsWithChildren & PageTitleProps> = (props: React.PropsWithChildren<PageTitleProps>) => {
-  const classes = useStyles();
-  return <div className={classes.content}>
-    <Typography variant={"h6"} align={"center"} className={classes.pageTitle}>
-      {props.title}
-    </Typography>
-    <Divider className={classes.divider} variant={"middle"} />
-    {props.children}
-  </div>;
+    return <Box sx={ styles.content }>
+        <Typography variant={"h6"} align={"center"} fontWeight={"bolder"}>
+            {props.title}
+        </Typography>
+        <Divider
+            sx={{
+                "marginTop": '4px',
+                "marginBottom": "8px",
+                "marginLeft": "80px",
+                "marginRight": "80px",
+                "height": 2
+            }}
+            variant={"middle"}/>
+        {props.children}
+    </Box>;
 }
 
 export const AppPageScaffold: FunctionComponent<React.PropsWithChildren & AppPageScaffoldProps> = (props: React.PropsWithChildren<AppPageScaffoldProps>) => {
-  const classes = useStyles();
 
-  function _content() {
-    if (props.title) {
-      // return title and divider with padding, followed by page content
-      return <PageTitle title={props.title}>{props.children}</PageTitle>;
+    function _content() {
+        if (props.title) {
+            // return title and divider with padding, followed by page content
+            return <PageTitle title={props.title}>{props.children}</PageTitle>;
+        }
+        // return just the page content
+        return <Box sx={ styles.content }>
+            {props.children}
+        </Box>;
     }
-    // return just the page content
-    return <div className={classes.content}>
-      {props.children}
-    </div>;
-  }
 
-  return (
-      <div>
-        <AppBanner/>
-        <DemoVersionBanner/>
-        {_content()}
-        <VersionString/>
-      </div>
-  );
+    return (
+        <div>
+            <AppBanner/>
+            <DemoVersionBanner/>
+            {_content()}
+            <VersionString/>
+        </div>
+    );
 }
